afb1d1c33a5b06b5d69406c3131619c86ac86a0d
Aldair <167644121+Alda16@users.noreply.github.com>
2026-04-28 19:45:36 -0600

    Actualización

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeIcon = document.getElementById('theme-icon');
     const body = document.body;
 
-    // 1. Comprobar si hay una preferencia guardada en localStorage
     const currentTheme = localStorage.getItem('theme');
 
-    // 2. Si hay una preferencia guardada, aplicarla
     if (currentTheme) {
         body.classList.add(currentTheme);
         if (currentTheme === 'dark-mode') {
             themeIcon.textContent = '☀️'; 
         }
     }
-    // 3. Escuchar el evento de clic en el botón
+
     themeToggleBtn.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
         let theme = 'light-mode'; 
@@ -23,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             themeIcon.textContent = '🌙'; 
         }
-        // 4. Guardar la preferencia en localStorage
         localStorage.setItem('theme', theme);
     });
 });
